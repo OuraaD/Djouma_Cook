87797ef05cf3dd8189ccbe31f798b42ed7015b20
OuraD <ouradiarra2@gmail.com>
2024-04-26 12:14:28 +0200

    vendredi

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,15 +8,10 @@ header.addEventListener("mouseleave", function () {
   header.classList.remove("visible");
 });
 
-
-
 const burgerI = document.getElementById("burger");
 
 burgerI.addEventListener("click", () => {
   const navigation = document.getElementById("nav");
   navigation.classList.toggle("active");
 });
-
-
-
 
